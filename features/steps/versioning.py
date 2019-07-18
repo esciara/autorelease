6dd02ee00c7baa33c431dc663f7846bab6585af4
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2019-07-19 01:58:34 +0200

    first version scenario creation of a starting repo working.

--- features/steps/versioning.py
+++ features/steps/versioning.py
@@ -1,44 +1,71 @@
 from behave import given, when, then, step
 from behave4cmd0 import command_steps
 from behave4cmd0 import textutil
+from behave4cmd0.pathutil import posixpath_normpath
 import datetime
+from git import Repo
+from hamcrest import *
+import os
 
 
-@given('a starting repo at version "{version}", with a staged file and a changelog file with:')
+# -----------------------------------------------------------------------------
+# STEPS: Git related steps
+# TYPE: multiple (@step)
+# -----------------------------------------------------------------------------
+@step('I wait for the CI/CD pipeline to complete successfully')
+def step_wait_for_pipeline_success(context):
+    raise NotImplementedError('STEP: Given I wait for the CI/CD pipeline to complete successfully')
+
+
+# -----------------------------------------------------------------------------
+# STEPS: Git related steps
+# TYPE: @given
+# -----------------------------------------------------------------------------
+@given('a starting repo at version "{version}", with a staged file and a changelog file with')
 def step_starting_repo_with_specific_change_log(context, version):
-    assert context.text is not None, "REQUIRE: multiline text"
-    raise NotImplementedError(
-        'STEP: Given a starting repo at version "{version}", with a staged file and a changelog file with:')
+    repo = context.repo = Repo.init(posixpath_normpath(context.workdir))
+    command_steps.step_a_file_named_filename_with(context, "ChangeLog.rst")
+    repo.index.add(["ChangeLog.rst"])
+    repo.index.commit("pipo bingo")
+    repo.create_tag("0.0.1")
+    context.surrogate_text = "Lorem ipsum"
+    command_steps.step_a_file_named_filename_with(context, "staged_file")
+    repo.index.add(["staged_file"])
 
 
 @given('a starting repo at version "{version}", with a staged file and a changelog file')
 def step_starting_repo(context, version):
-    context.text = "Anything I want"
+    context.surrogate_text = """
+0.0.1 (2019-06-12)
+------------------
+
+New
+~~~
+- something new. [Geronimo]
+"""
     step_starting_repo_with_specific_change_log(context, version)
 
 
-@then('a repo named "{repo_prefix}" ending with today\'s date should exist')
-def step_repo_exists(context, repo_prefix):
-    raise NotImplementedError(
-        'STEP: Then I should have a repo named "autorelease-test-repo-" ending with today\'s date')
-
-
-@then('the last commit should have label "{version}"')
-def step_last_commit_labelled(context, version):
-    raise NotImplementedError('STEP: Then I should have a git label "{version}" at the last commit')
-
-
-@then('the file "{filename}" should be staged in git')
-def step_file_staged(context, filename):
-    raise NotImplementedError('STEP: Then I should have a file named "staged_file" staged in git')
-
-
-@given('I commit the staged file with commit message:')
+@given('I commit the staged file with commit message')
 def step_commit_with_message(context):
     assert context.text is not None, "REQUIRE: multiline text"
     raise NotImplementedError('STEP: Given I commit the staged file with commit message')
 
 
+@given('the current branch is "{branch_name}"')
+def step_current_branch_is(context, branch_name):
+    raise NotImplementedError('STEP: Given the current branch is "pr_branch"')
+
+
+@given('I create a PR from "{source_branch_name}" to "{target_branch_name}"')
+def step_create_pr(context, source_branch_name, target_branch_name):
+    raise NotImplementedError('STEP: Given I create a PR from "pr_branch" to "master"')
+
+
+# -----------------------------------------------------------------------------
+# STEPS: Git related steps
+# TYPE: @when
+# -----------------------------------------------------------------------------
 @when('I bump the version')
 def step_bump_version(context):
     raise NotImplementedError('STEP: When I bump the version')
@@ -54,7 +81,50 @@ def step_generate_change_log(context):
     raise NotImplementedError('STEP: When I generate the change log')
 
 
-@then('the file "{filename}" should contain (templated):')
+@when('I merge the PR')
+def step_merge_pr(context):
+    raise NotImplementedError('STEP: When I merge the PR')
+
+
+# -----------------------------------------------------------------------------
+# STEPS: Git related steps
+# TYPE: @then
+# -----------------------------------------------------------------------------
+@then('a local repo should exist')
+def step_repo_exists(context):
+    try:
+        assert_that(context.repo, not_none(), "The repo should exist.")
+    except AttributeError:
+        assert False, "The repo should exist."
+
+
+@then('a repo named "{repo_prefix}" ending with today\'s date should exist on GitLab')
+def step_gitlab_repo_exists(context, repo_prefix):
+    raise NotImplementedError(
+        'STEP: Then I should have a repo named "autorelease-test-repo-" ending with today\'s date')
+
+
+@then('the local repo should have "{num_of_commits}" commit')
+def step_impl(context, num_of_commits):
+    assert_that(list(context.repo.iter_commits()), has_length(1))
+
+
+@then('the last commit should have be tagged "{tag_name}"')
+def step_last_commit_labelled(context, tag_name):
+    assert_that(context.repo.tags[tag_name].commit, equal_to(context.repo.head.commit))
+
+
+@then('the file "{filename}" should be in the last commit')
+def step_file_staged(context, filename):
+    assert_that(context.repo.head.commit.stats.files, has_key(filename))
+
+
+@then('the file "{filename}" should be staged in git')
+def step_file_staged(context, filename):
+    assert_that([item.a_path for item in context.repo.index.diff("HEAD")], contains(filename))
+
+
+@then('the file "{filename}" should contain (templated)')
 def step_file_should_contain_multiline_text_templated(context, filename):
     assert context.text is not None, "REQUIRE: multiline text"
     expected_text = context.text
@@ -64,26 +134,6 @@ def step_file_should_contain_multiline_text_templated(context, filename):
                                                      __GIT_COMMITER__="TO DO"
                                                      )
     command_steps.step_file_should_contain_text(context, filename, expected_text)
-
-
-@given('the current branch is "{branch_name}"')
-def step_current_branch_is(context, branch_name):
-    raise NotImplementedError('STEP: Given the current branch is "pr_branch"')
-
-
-@given('I create a PR from "{source_branch_name}" to "{target_branch_name}"')
-def step_create_pr(context, source_branch_name, target_branch_name):
-    raise NotImplementedError('STEP: Given I create a PR from "pr_branch" to "master"')
-
-
-@step('I wait for the CI/CD pipeline to complete successfully')
-def step_wait_for_pipeline_success(context):
-    raise NotImplementedError('STEP: Given I wait for the CI/CD pipeline to complete successfully')
-
-
-@when('I merge the PR')
-def step_merge_pr(context):
-    raise NotImplementedError('STEP: When I merge the PR')
 
 
 @then('the file "{filename}" should have been changed in the last commit')
